9b579747efe6ce5259d2b45b4c3e4f8ab141c4e7
Krzysi3k <global.takeover@o2.pl>
2022-06-02 00:48:53 +0200

    port

--- app.py
+++ app.py
@@ -84,4 +84,4 @@ def get_redis_data(data: str):
 
 
 if __name__ == '__main__':
-   uvicorn.run(app=app, host='0.0.0.0', port=5003, log_level='error')
+   uvicorn.run(app=app, host='0.0.0.0', port=5000, log_level='error')
